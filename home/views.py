4658d999f819cc9be29d73fe1cf0e360b2c55d33
Zai <zaiyellyintaung@gmail.com>
2023-05-16 16:41:21 +0630

    rest after sign in up out

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def index(request):
@@ -41,6 +46,19 @@ def signout(request):
 def register(request):
 	templates="home/register.html"
 
+	if request.method=="POST":
+		form=UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username=form.cleaned_data['username']
+			password=form.cleaned_data['password1']
+			user=authenticate(request,username=username,password=password)
+			signin(request,user)
+			messages.success(request,("Please login "))
+			return redirect('loginpage')
+		else:
+			messages.success(request,("Not okay"))
+			return redirect('registerpage')
 	data={
 		"title":"Register"
 	}
