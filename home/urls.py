fd7dc4fcac17000fe4ef89846730d908213116b3
Zai <zaiyellyintaung@gmail.com>
2023-05-16 16:02:33 +0630

    Done sign in out and navbar

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+	path("home/",views.index,name="homepage"),
+	path("about/",views.about,name="aboutpage"),
+	path("signin/",views.signin,name="signinpage"),
+	path("signout/",views.signout,name="signoutpage"),
+	path("register/",views.register,name="registerpage"),
+
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	templates="home/index.html"
+
+	data={
+		"title":"Home"
+	}
+
+	return render(request,templates,data)
+
+def about(request):
+	templates="home/about.html"
+
+	data={
+		"title":"About"
+	}
+
+	return render(request,templates,data)
+
+def signin(request):
+	templates="home/signin.html"
+
+	data={
+		"title":"Sign in"
+	}
+
+	return render(request,templates,data)
+
+def signout(request):
+	templates="home/signout.html"
+
+	data={
+		"title":"Sign out"
+	}
+
+	return render(request,templates,data)
+
+
+def register(request):
+	templates="home/register.html"
+
+	data={
+		"title":"Register"
+	}
+
+	return render(request,templates,data)
